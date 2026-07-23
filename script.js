034ee534ed2f57712612424c4e92466b629925da
nicolondonoe <nicolondonoe@gmail.com>
2026-07-22 19:11:30 -0500

    Add button to customize grid

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const container = document.querySelector("div.container");
 let gridSquares = [];
+let sizeButton = document.querySelector("button");
+let size = 16;
 
-for (let i = 0; i < 256; i++) {
-  gridSquares[i] = document.createElement("div");
-  gridSquares[i].classList.add("square");
+function getSize() {
+  size = prompt("Size of one side: ");
+  if (size > 100) {
+    size = prompt("Size must not be bigger than 100, enter new number");
+  }
+  console.log(size);
+  createSquares();
 }
 
-gridSquares.forEach((square) => container.appendChild(square));
+function createSquares() {
 
-gridSquares.forEach((square) => square.addEventListener("mouseenter", () => square.style.backgroundColor = "blue"));
+  gridSquares.forEach((square) => container.removeChild(square));
+  gridSquares = [];
+
+  container.style.width = 16*size + 'px';
+
+  for (let i = 0; i < size**2; i++) {
+    gridSquares[i] = document.createElement("div");
+    gridSquares[i].classList.add("square");
+  }
+  
+  gridSquares.forEach((square) => container.appendChild(square));
+
+  gridSquares.forEach((square) => square.addEventListener("mouseenter", () => square.style.backgroundColor = "blue"));
+
+}
+
+
+sizeButton.addEventListener("click", getSize);
+
+createSquares();
